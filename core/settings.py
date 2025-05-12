9fe417ba0beb6a9b71748ce1525e6f4f143818fe
Maria <mariahelenmhlc@gmail.com>
2025-05-11 23:12:56 -0300

    Ajuste barras django admin

--- core/settings.py
+++ core/settings.py
@@ -147,6 +147,40 @@ JAZZMIN_SETTINGS = {
     # "show_ui_builder": False,
 }
 
+# JAZZMIN_UI_TWEAKS = {
+#     "navbar_small_text": False,
+#     "footer_small_text": False,
+#     "body_small_text": False,
+#     "brand_small_text": False,
+#     "brand_colour": "navbar-gray",
+#     "accent": "accent-primary",
+#     "navbar": "navbar-white navbar-light",
+#     "no_navbar_border": False,
+#     "navbar_fixed": False,
+#     "layout_boxed": False,
+#     "footer_fixed": False,
+#     "sidebar_fixed": True,
+#     "sidebar": "sidebar-dark-lightblue",
+#     "sidebar_nav_small_text": False,
+#     "sidebar_disable_expand": False,
+#     "sidebar_nav_child_indent": False,
+#     "sidebar_nav_compact_style": True,
+#     "sidebar_nav_legacy_style": False,
+#     "sidebar_nav_flat_style": True,
+#     "theme": "cerulean",
+#     "dark_mode_theme": None,
+#     "button_classes": {
+#         "primary": "btn-outline-primary",
+#         "secondary": "btn-outline-secondary",
+#         "info": "btn-outline-info",
+#         "warning": "btn-outline-warning",
+#         "danger": "btn-outline-danger",
+#         "success": "btn-outline-success"
+#     },
+#     "actions_sticky_top": False
+# }
+
+
 JAZZMIN_UI_TWEAKS = {
     "navbar_small_text": False,
     "footer_small_text": False,
@@ -156,9 +190,9 @@ JAZZMIN_UI_TWEAKS = {
     "accent": "accent-primary",
     "navbar": "navbar-white navbar-light",
     "no_navbar_border": False,
-    "navbar_fixed": False,
+    "navbar_fixed": True,
     "layout_boxed": False,
-    "footer_fixed": False,
+    "footer_fixed": True,
     "sidebar_fixed": True,
     "sidebar": "sidebar-dark-lightblue",
     "sidebar_nav_small_text": False,
